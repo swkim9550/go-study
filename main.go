bb794c7be48b6076dcd0a71481a2ec6b645019c7
swkim9550 <kbkkim2@naver.com>
2022-03-20 18:54:47 +0900

    feat : if-else

--- main.go
+++ main.go
@@ -6,60 +6,65 @@ import (
 	// "github.com/serranoarevalo/something"
 )
 
-func lenAndUpper(name string)(int,string){
+//multi return
+func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
-func multply(a ,b int) int{
+// a,b int 형 인자 받기
+// return 값은 int
+func multply(a, b int) int {
 	return a * b
 }
 
-func repeatMe(words ...string){
-	defer fmt.Println("reapeat Me end ...") //메소드가 끝나면 실행된다.
+//defer 메소드가 끝나면 실행 된다
+// ... 여러 인자를 받을 수 있다
+//repeatMe("test1","test1","test1","test1","test1")
+func repeatMe(words ...string) {
+	defer fmt.Println("reapeat Me end ...")
 	fmt.Println(words)
 }
 
 //for each
+//result := superAdd(1, 2, 3, 4, 5, 6)
 func superAdd(numbers ...int) int {
 	defer fmt.Println("super Add end .. ")
 	total := 0
-	for _ ,number := range numbers {
-		total += number		
+	for _, number := range numbers {
+		total += number
 	}
-	return total;
+	return total
 }
 
 //for 문
 func superAddV2(numbers ...int) int {
 	defer fmt.Println("super Add  V2 end .. ")
-	for i:=0 ; i< len(numbers); i++{
+	for i := 0; i < len(numbers); i++ {
 		fmt.Println(numbers[i])
 	}
-	return 1;
+	return 1
 }
 
+// if -else
+// else 는 따로 써주지 않아도 된다.
+func canIdrink(age int) bool {
+	if age < 18 {
+		return false
+	}
+	return true
+
+}
+
+// if -else
+// if else 안에서 variable expression 사용
+func canIdrinkV2(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+
+}
 
 func main() {
-	result := superAdd(1,2,3,4,5,6)
-	fmt.Println(result)
-
-	//superAddV2(1,2,3,4,5)
-
-
-	//totalLenght, upperName := lenAndUpper("seongwoo")
-	//fmt.Println(totalLenght,upperName)
-
-	//totalLenght2, _ := lenAndUpper("seongwoo")
-	//fmt.Println(totalLenght2)
-
-	//repeatMe("test1","test1","test1","test1","test1")
-
-
-	// fmt.Println(multply(2,2))
-	// name := "test"
-	// //var name string = "seongwoo"
-	// name = "lynn"
-	// fmt.Println(name)
+	fmt.Println(canIdrink(18))
 }
-
-
